2512e1822eee38bf76aee582a63eb40a1a2470cc
胡贵山 <13450737557@163.com>
2019-04-15 17:48:53 +0800

    update

--- src/views/good/Good.vue
+++ src/views/good/Good.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="book">
-    <SearchBox placeholder="搜索物品名称" @search="search" class="s"/>
-    <Swiper class="book-list">
+    <SearchBox placeholder="搜索物品名称" @search="search" :class="{s: !haveType}"/>
+    <!-- 无类型 -->
+    <Swiper class="book-list" v-if="!haveType">
       <!-- <template v-slot:options>
         <div class="options">
           <div
@@ -28,6 +29,41 @@
         </div>
       </template>
     </Swiper>
+    <!-- 有类型 -->
+    <Swiper class="book-list" v-if="haveType">
+      <template v-slot:options>
+        <div class="options">
+          <div
+            v-for="(type,index) in data.data"
+            :key="type.goods_type"
+            :class="['option', {actived: currentIndex == index}]"
+            :data-index="index"
+            @click="toggleType"
+          >{{ type["goods_type_name"] }}</div>
+        </div>
+      </template>
+      <template v-slot:sliderBox>
+        <div
+          class="slider-box"
+          :style="{width: data.total ===0? '100%': `${data.total*100}%`,transform: data.total === 0? 'translateX(0)': `translateX(${-currentIndex*(100/data.total)}%)`}"
+        >
+          <Nothing v-show="data.total === 0"/>
+          <div
+            v-for="(type) in data.data"
+            :key="type.goods_type"
+            class="swiper-item"
+            :style="{width: `${100/(data.total)}%`}"
+          >
+            <div class="list">
+              <Nothing v-show="isNothing"/>
+              <template v-for="item in type.data">
+                <BringItem :key="item.goods_id" :list-item="item" @mask="mask" @borrow="borrow"/>
+              </template>
+            </div>
+          </div>
+        </div>
+      </template>
+    </Swiper>
     <Cart
       class="c"
       :popuped="maskActived"
@@ -40,15 +76,18 @@
   </div>
 </template>
 <script>
-import SearchBox from '../../components/SearchBox.vue'
-import Swiper from '../../components/swiper/Swiper.vue'
+import SearchBox from "../../components/SearchBox.vue";
+import Swiper from "../../components/swiper/Swiper.vue";
 // import BorrowItem from '../../components/swiper/BorrowItem.vue'
-import BringItem from '../../components/swiper/BringItem.vue'
-import Cart from '../../components/Cart.vue'
-import Nothing from "../../components/Nothing.vue"
-import { receive_goods,get_goods_list_by_assets } from '../../utils/request.js'
+import BringItem from "../../components/swiper/BringItem.vue";
+import Cart from "../../components/Cart.vue";
+import Nothing from "../../components/Nothing.vue";
+import {
+  receive_goods,
+  get_goods_list_by_assets
+} from "../../utils/request.js";
 export default {
-  name: 'Book',
+  name: "Book",
   components: {
     Swiper,
     SearchBox,
@@ -56,17 +95,18 @@ export default {
     Cart,
     Nothing
   },
-  created () {
+  created() {
     // this.data = JSON.parse(JSON.stringify(this.originData))
-    this.init("加载中")
+    this.init("加载中");
   },
-  data () {
+  data() {
     return {
       originData: [],
       data: [],
       currentIndex: 0,
-      maskActived: false
-    }
+      maskActived: false,
+      haveType: true
+    };
   },
   computed: {
     isNothing() {
@@ -75,16 +115,149 @@ export default {
   },
   methods: {
     async init(tip) {
-        this.$loading.start(tip)
-    let response = await get_goods_list_by_assets({
-      assets_id: 5,
-      // floor_name: this.$store.state.floorName,
-      has_stock: true
-    })
-    let arr = response.data.data.data[0].data;
+      this.$loading.start(tip);
+      let response = await get_goods_list_by_assets({
+        assets_id: 5,
+        // floor_name: this.$store.state.floorName,
+        has_stock: true
+      });
+      if (
+        response.data.data.total === 1 &&
+        response.data.data.data[0].goods_type === -1
+      ) {
+        this.haveType = false;
+        let arr = response.data.data.data[0].data;
+        if (response.data.state === 0) {
+          this.originData = arr;
+          this.data = JSON.parse(JSON.stringify(arr));
+        } else {
+          this.$notify({
+            message: response.data.msg,
+            duration: 100,
+            status: "danger"
+          });
+        }
+        this.$loading.close();
+      } else {
+        this.haveType = true;
+        this.originData = response.data.data;
+        this.data = JSON.parse(JSON.stringify(response.data.data));
+        this.$loading.close();
+      }
+    },
+    toggleType(e) {
+      let index = e.target.dataset["index"];
+      this.currentIndex = index;
+    },
+    mask() {
+      this.maskActived = true;
+    },
+    borrow(borrowInfo) {
+      this.currentGoodId = borrowInfo.goodId;
+    },
+    clearMask() {
+      this.maskActived = false;
+    },
+    popupItems() {
+      this.maskActived = true;
+    },
+    search(keyword) {
+      let _k = keyword.trim();
+      if (_k === "") {
+        this.currentIndex = 0
+        this.init("数据重新加载中 ");
+        // this.data = [...this.originData];
+        return;
+      }
+      if (!this.haveType) {
+        this.data = this.originData.filter(item => {
+          return item.goods_name.indexOf(_k) !== -1;
+        });
+      } else {
+        let data = this.originData.data
+          .filter(type => {
+            return (
+              type.data.find(item => {
+                return item.goods_name.indexOf(_k) !== -1; // 筛选条件
+              }) !== undefined
+            );
+          })
+          .map(type => {
+            return {
+              data: type.data.filter(item => {
+                return item.goods_name.indexOf(_k) !== -1;
+              }),
+              goods_type: type.goods_type,
+              goods_type_name: type.goods_type_name
+            };
+          });
+        this.data = {
+          data,
+          total: data.length
+        };
+      }
+    },
+    _isExceed(goodInfo) {
+      if (!this.haveType) {
+        let temp = this.data.find(item => {
+          return `${item.goods_id}` === goodInfo.goods_id;
+        });
+
+        return (
+          this.$store.state.cart.c[goodInfo.goods_id]["num"] >=
+          temp.remaining_stock
+        );
+      } else {
+        let temp = this.data.data
+          .find(type => {
+            return (
+              type.data.find(item => {
+                return `${item.goods_id}` === goodInfo.goods_id;
+              }) !== undefined
+            );
+          })
+          .data.find(item => {
+            return `${item.goods_id}` === goodInfo.goods_id;
+          });
+
+        return (
+          this.$store.state.cart.c[goodInfo.goods_id]["num"] >=
+          temp.remaining_stock
+        );
+      }
+    },
+    add(goodInfo) {
+      if (this._isExceed(goodInfo)) return;
+      this.$store.commit("cart/add", goodInfo);
+    },
+    async ok() {
+      this.maskActived = false;
+      let goods_id = Object.keys(this.$store.state.cart.c).join(",");
+      let lent_count = Object.keys(this.$store.state.cart.c)
+        .map(code => {
+          return this.$store.state.cart.c[code]["num"];
+        })
+        .join(",");
+      if (goods_id === "" || lent_count === "") {
+        this.$notify({
+          message: "你还没有选呢U•ェ•*U",
+          duration: 100,
+          status: "danger"
+        });
+        return;
+      }
+      this.$loading.start("处理中");
+      let response = await receive_goods({
+        goods_id,
+        lent_count,
+        lent_name: this.$store.state.username,
+        lent_code: this.$store.state.workCode
+      });
       if (response.data.state === 0) {
-        this.originData = arr;
-        this.data = JSON.parse(JSON.stringify(arr));
+        this.$notify({
+          message: response.data.msg,
+          duration: 100
+        });
       } else {
         this.$notify({
           message: response.data.msg,
@@ -92,88 +265,15 @@ export default {
           status: "danger"
         });
       }
-    this.$loading.close()
-    },
-    toggleType (e) {
-      let index = e.target.dataset['index']
-      this.currentIndex = index
-    },
-    mask () {
-      this.maskActived = true
-    },
-    borrow (borrowInfo) {
-      this.currentGoodId = borrowInfo.goodId
-    },
-    clearMask () {
-      this.maskActived = false
-    },
-    popupItems () {
-      this.maskActived = true
-    },
-    search (keyword) {
-       let _k = keyword.trim();
-      if (_k === "") {
-        this.init("数据重新加载中 ");
-        // this.data = [...this.originData];
-        return;
-      }
-      this.data = this.originData.filter(item => {
-        return item.goods_name.indexOf(_k) !== -1;
-      });
-    },
-    _isExceed(goodInfo) {
-      let temp = this.data.find(item=> {
-        return `${item.goods_id}` === goodInfo.goods_id
-      })
-
-      return this.$store.state.cart.c[goodInfo.goods_id]['num'] >= temp.remaining_stock
-    },
-    add(goodInfo) {
-      if(this._isExceed(goodInfo)) return;
-      this.$store.commit('cart/add', goodInfo)
-    },
-    async ok() {
-      this.maskActived = false
-      let goods_id = Object.keys(this.$store.state.cart.c).join(",")
-      let lent_count = Object.keys(this.$store.state.cart.c).map(code=> {
-        return this.$store.state.cart.c[code]["num"]
-      }).join(",")
-      if(goods_id === "" || lent_count === "") {
-        this.$notify({
-          message: "你还没有选呢U•ェ•*U",
-          duration: 100,
-          status: "danger"
-        })
-        return
-      }
-      this.$loading.start("处理中")
-      let response = await receive_goods({
-        goods_id,
-        lent_count,
-        lent_name: this.$store.state.username,
-        lent_code: this.$store.state.workCode
-      })
-      if(response.data.state === 0) {
-        this.$notify({
-          message: response.data.msg,
-          duration: 100
-        })
-      }else {
-        this.$notify({
-          message: response.data.msg,
-          duration: 100,
-          status: "danger"
-        })
-      }
-      this.$loading.close()
+      this.$loading.close();
       // this.originData.data.find(item=> {
       //   item.data.find()
       // })
-      this.$store.commit('cart/clear')
-      this.init("数据重新加载中")
+      this.$store.commit("cart/clear");
+      this.init("数据重新加载中");
     }
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 .book {
